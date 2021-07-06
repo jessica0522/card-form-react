8ce47af7cf66981cac655192bae680c615f81c00
jessica0522 <jessica040532@gmail.com>
2021-07-07 09:02:57 +1200

    add header for Menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import arrow from '../assets/arrow.svg';
 
 const Menu = () => {
   return (
     <div className="container">
-      Menu
+      <div className="header">
+        <div className="hamburger-container">
+          <img className="hamburger" src={arrow} alt="arrow-icon" />
+        </div>
+        <div className="title-container">
+          <h3 className="tc">Menu</h3>
+        </div>
+      </div>
     </div>
   )
 }
