2ade98305286b7017201fc7a280ffaba363256da
jessica0522 <jessica040532@gmail.com>
2021-07-07 09:01:19 +1200

    add header in Register component

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import hamburger from '../assets/hamburger.svg';
 
 const Register = () => {
   return (
     <div className="container">
-      Register
+      <div className="header">
+        <div className="hamburger-container">
+          <img className="hamburger" src={hamburger} alt="hamburger-icon" />
+        </div>
+        <div className="title-container">
+          <h3 className="tc">Register card form</h3>
+        </div>
+      </div>
     </div>
   )
 }
