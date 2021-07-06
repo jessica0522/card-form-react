26e9bef337055267f1b73cf4d0ec59941d6b5339
jessica0522 <jessica040532@gmail.com>
2021-07-07 08:50:19 +1200

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import './App.css';
+import Menu from './components/Menu'
+import Register from './components/Register'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/menu" >
+            <Menu />
+          </Route>
+          <Route path="/" >
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
