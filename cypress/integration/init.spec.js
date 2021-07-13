9558e8c78a7e27cd9477a43cf59d7c405ef6da10
jessica0522 <jessica040532@gmail.com>
2021-07-13 14:26:57 +1200

    change cypress localhost port to 3000

--- cypress/integration/init.spec.js
+++ cypress/integration/init.spec.js
@@ -1,7 +1,7 @@
 
 describe('card form with react', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3001')
+    cy.visit('http://localhost:3000')
   })
 
   it('display register page by default', () => {
